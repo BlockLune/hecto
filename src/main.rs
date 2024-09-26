506443103cceec553ca70cea037bb60560433922
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-09-26 09:19:41 +0800

    feat: enable raw mode of the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use crossterm::terminal::disable_raw_mode;
+use crossterm::terminal::enable_raw_mode;
+
+use std::io;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // enable raw mode of the terminal
+    // i.e. disable the cooked mode of the terminal
+    enable_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+    }
 }
